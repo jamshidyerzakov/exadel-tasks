7964fb099dece572a6d8937fcdea1a7bd4b8b164
Jamshid Yerzakov <jamshidyerzakov@gmail.com>
2022-02-21 11:04:55 +0500

    [add]: links and script

---
+++ Task2/README.md
@@ -0,0 +1,17 @@
+# Task 2 for exadel internship
+
+## Url to the webpage and s3 object below
+
+### Webpage
+- Hello page -> http://16.171.41.121/hello.html 
+- Info page -> http://16.171.41.121/info.html
+
+### S3 objects
+- Info page -> https://tesk2-screens.s3.eu-north-1.amazonaws.com/Screen+Shot+2022-02-21+at+10.56.20.png
+- Hello page -> https://tesk2-screens.s3.eu-north-1.amazonaws.com/Screen+Shot+2022-02-21+at+10.56.32.png
+
+### Run remote script to install nginx and generate pages
+
+```bash
+$ ssh $SUDOER@$PUBLIC_IP "bash -s" -- < ./script.sh
+```
---
+++ Task2/script.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+HELLO_PAGE=/var/www/html/hello.html
+INFO_PAGE=/var/www/html/info.html
+OS_INFO=$(cat /etc/os-release)
+MEMORY_INFO=$(free -h)
+DISK_INFO=$(df -H)
+PROCESS_INFO=$(ps aux)
+
+
+function install_nginx() {
+        sudo apt install nginx
+        sudo chown -R ubuntu:ubuntu /var/www/html/
+}
+
+function create_info_page() {
+        cat > $INFO_PAGE << EOF
+        <!DOCTYPE html>
+        <html>
+          <head>
+             <title>New Page</title>
+          </head>
+          <body>
+          <pre>
+          <h1> OS info </h1>
+          <pre>$OS_INFO</pre>
+          <h1> Memory info </h1>
+          <pre>$MEMORY_INFO</pre>
+          <h1> Disk info </h1>
+          <pre>$DISK_INFO</pre>
+          <h1> Process info </h1>
+          <pre>$PROCESS_INFO</pre>
+          </body>
+        </html>
+EOF
+}
+
+function create_hello_page() {
+        cat > $HELLO_PAGE << EOF
+        <!DOCTYPE html>
+        <html>
+          <head>
+              <title>New Page</title>
+                </head>
+                  <body>
+          <pre>
+          <h1>HELLO WORLD </h1>
+          <hr>
+          </body>
+        </html>
+
+EOF
+}
+
+install_nginx
+create_info_page
+create_hello_page
