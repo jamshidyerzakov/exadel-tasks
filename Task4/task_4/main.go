7a10df1926ede45fbe18eaf6f33f4204332f375b
Jamshid Yerzakov <jamshidyerzakov@gmail.com>
2022-03-03 17:52:57 +0500

    add task4

---
+++ Task4/task_4/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Go Server is running!")
+
+	PORT := ":8080"
+
+	fmt.Printf("Server is running on %s", PORT)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello WEB APP")
+
+		fmt.Println("User entered to root")
+	})
+
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
